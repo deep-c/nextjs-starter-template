701cbd63add0a1f984a6941c44da58fbef14cf01
Deeptesh Chagan <deepc28@gmail.com>
2020-07-26 17:14:34 +1000

    add basic nextAuth

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,23 +1,23 @@
-import Adapters from "next-auth/adapters"
+import {TypeORM} from "next-auth/adapters"
 
 // Extend the built-in models using class inheritance
-export default class User extends Adapters.TypeORM.Models.User.model {
-  // You can extend the options in a model but you should not remove the base
-  // properties or change the order of the built-in options on the constructor
-  constructor(name, email, image, emailVerified) {
-    super(name, email, image, emailVerified)
-  }
+export default class User extends TypeORM.Models.User.model {
+    // You can extend the options in a model but you should not remove the base
+    // properties or change the order of the built-in options on the constructor
+    constructor(name, email, image, emailVerified) {
+        super(name, email, image, emailVerified)
+    }
 }
 
 export const UserSchema = {
-  name: "User",
-  target: User,
-  columns: {
-    ...Adapters.TypeORM.Models.User.schema.columns,
-    // Adds a phoneNumber to the User schema
-    phoneNumber: {
-      type: "varchar",
-      nullable: true,
-    },
-  },
+    name: "User",
+    target: User,
+    columns: {
+        ...TypeORM.Models.User.schema.columns,
+        // Adds a phoneNumber to the User schema
+        phoneNumber: {
+            type: "varchar",
+            nullable: true
+        }
+    }
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,41 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import {NextApiRequest, NextApiResponse} from 'next'
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
 import Adapters from "next-auth/adapters"
-import Models from "models"
+import ormconfig from '@project/ormconfig.json'
+import Models from 'models'
 
-const options = {
-
+// Construct our own object so that we dont use the path references for
+// entities, migrations etc since those are relative and wont work if passed.
+const dbConfig = {
+    type: ormconfig.type,
+    host: ormconfig.host,
+    port: ormconfig.port,
+    username: ormconfig.username,
+    password: ormconfig.password,
+    database: ormconfig.database,
+    synchronize: ormconfig.synchronize,
 }
 
-export default (req, res) => NextAuth(req, res, options)
+const options = {
+    providers: [
+        Providers.Auth0({
+            clientId: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            domain: process.env.AUTH0_DOMAIN,
+        }),
+    ],
+    adapter: Adapters.TypeORM.Adapter(
+        dbConfig,
+        {
+            models: {
+                User: Models.User,
+            },
+        }
+    ),
+    secret: process.env.SECRET,
+    debug: process.env.DEBUG,
+    database: dbConfig,
+}
+
+export default(req : NextApiRequest, res : NextApiResponse) => NextAuth(req, res, options)
