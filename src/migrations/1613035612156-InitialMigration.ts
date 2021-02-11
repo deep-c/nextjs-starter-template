6a2405e9b17909a0bd4f03a28cdb75b745fb6823
Deeptesh Chagan <deepc28@gmail.com>
2021-02-11 20:58:36 +1100

    fix migration file

--- src/migrations/1613035612156-InitialMigration.ts
+++ src/migrations/1613035612156-InitialMigration.ts
@@ -8,9 +8,6 @@ export class InitialMigration1613035612156 implements MigrationInterface {
         await queryRunner.query(`CREATE INDEX "userId" ON "account" ("userId") `);
         await queryRunner.query(`CREATE INDEX "providerId" ON "account" ("providerId") `);
         await queryRunner.query(`CREATE INDEX "providerAccountId" ON "account" ("providerAccountId") `);
-        await queryRunner.query(`CREATE INDEX "userId" ON "account" ("userId") `);
-        await queryRunner.query(`CREATE INDEX "providerId" ON "account" ("providerId") `);
-        await queryRunner.query(`CREATE INDEX "providerAccountId" ON "account" ("providerAccountId") `);
         await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying, "email" character varying, "emailVerified" TIMESTAMP, "image" character varying, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "dob" TIMESTAMP, CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "session" ("id" SERIAL NOT NULL, "userId" integer NOT NULL, "expires" TIMESTAMP NOT NULL, "sessionToken" character varying NOT NULL, "accessToken" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_31ee7107a0ed316df2086cbf5f1" UNIQUE ("sessionToken"), CONSTRAINT "UQ_69ed5ce783e0c16540f12630a00" UNIQUE ("accessToken"), CONSTRAINT "UQ_31ee7107a0ed316df2086cbf5f1" UNIQUE ("sessionToken"), CONSTRAINT "UQ_69ed5ce783e0c16540f12630a00" UNIQUE ("accessToken"), CONSTRAINT "PK_f55da76ac1c3ac420f444d2ff11" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "verification_request" ("id" SERIAL NOT NULL, "identifier" character varying NOT NULL, "token" character varying NOT NULL, "expires" TIMESTAMP NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_5ebed08ac78d30f9b04a2c4bcba" UNIQUE ("token"), CONSTRAINT "UQ_5ebed08ac78d30f9b04a2c4bcba" UNIQUE ("token"), CONSTRAINT "PK_9d9499e0fabae343c7ec3ecfac9" PRIMARY KEY ("id"))`);
@@ -20,9 +17,6 @@ export class InitialMigration1613035612156 implements MigrationInterface {
         await queryRunner.query(`DROP TABLE "verification_request"`);
         await queryRunner.query(`DROP TABLE "session"`);
         await queryRunner.query(`DROP TABLE "user"`);
-        await queryRunner.query(`DROP INDEX "providerAccountId"`);
-        await queryRunner.query(`DROP INDEX "providerId"`);
-        await queryRunner.query(`DROP INDEX "userId"`);
         await queryRunner.query(`DROP INDEX "providerAccountId"`);
         await queryRunner.query(`DROP INDEX "providerId"`);
         await queryRunner.query(`DROP INDEX "userId"`);
